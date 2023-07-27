5d8cb8f47730e53723aeee59e73b902c4d410c06
Purinat Sanbundit <37452245+PurinatG-01@users.noreply.github.com>
2023-07-27 17:12:33 +0700

    feat: clear home page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div className="p-4 flex items-center justify-center">
+        <section className="login flex flex-col gap-4 rounded-xl p-4 border border-slate-700">
+        <form action="" className="login__form">
+            
+        </form>
+        </section>
+    </div>
+  )
+}

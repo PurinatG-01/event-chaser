4385d136aca312ef4fc5edb778f4a66cd13f6d5f
Purinat Sanbundit <37452245+PurinatG-01@users.noreply.github.com>
2023-07-29 17:42:58 +0700

    feat: update landing page

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,7 +1,9 @@
+"use client"
 import { Container } from "@nextui-org/react"
 import React, { useEffect } from "react"
 import EventGridList from "~/components/EventGridList"
 import useEventList from "~/hooks/useEventList"
+import MainLayout from "~/layouts/MainLayout"
 
 export default function ListPage() {
   const { eventList, queryEventList } = useEventList()
@@ -9,10 +11,12 @@ export default function ListPage() {
     queryEventList(1)
   }, [])
   return (
-    <div className="flex flex-col">
-      <Container className="container py-4 !max-w-[1400px]">
-        <EventGridList list={eventList} />
-      </Container>
-    </div>
+    <MainLayout>
+      <div className="flex flex-col">
+        <Container className="container py-4 !max-w-[1400px]">
+          <EventGridList list={eventList} />
+        </Container>
+      </div>
+    </MainLayout>
   )
 }
--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -3,21 +3,59 @@ import { Button, Navbar, Link, Text, Avatar } from "@nextui-org/react"
 import { useRouter, usePathname } from "next/navigation"
 import useUser from "~/hooks/useUser"
 
+const navList = [
+  { title: "Home", link: "/" },
+  { title: "List", link: "/list" },
+]
 export default function MainNavbar() {
   const path = usePathname()
   const { username, isLogin, avatarUrl } = useUser()
   return path?.includes("/login") ? null : (
-    <Navbar className="!shadow-none" variant="sticky">
+    <Navbar bordered variant="sticky" className="!shadow-none">
       <Navbar.Brand>
+        <Navbar.Toggle
+          showIn="xs"
+          css={{
+            marginRight: "0.5rem",
+          }}
+          aria-label="toggle navigation"
+        />
         <Text b color="inherit">
           Event Chaser
         </Text>
       </Navbar.Brand>
-      <Navbar.Content hideIn="xs"></Navbar.Content>
+      <Navbar.Content hideIn="xs">
+        {navList.map((item, index) => (
+          <Navbar.Link key={index} href={item.link}>
+            {item.title}
+          </Navbar.Link>
+        ))}
+      </Navbar.Content>
+      <Navbar.Collapse className="!mr-4">
+        {navList.map((item, index) => (
+          <Navbar.CollapseItem key={index}>
+            <Link
+              color="inherit"
+              css={{
+                minWidth: "100%",
+              }}
+              href={item.link}
+            >
+              {item.title}
+            </Link>
+          </Navbar.CollapseItem>
+        ))}
+      </Navbar.Collapse>
       <Navbar.Content>
         {isLogin ? (
           <Navbar.Link href="#">
-            <Avatar src={avatarUrl} size="sm" className="!mr-2" /> {username}
+            <Avatar
+              color="gradient"
+              src={avatarUrl}
+              size="sm"
+              className="!mr-2"
+            />{" "}
+            {username}
           </Navbar.Link>
         ) : (
           <Navbar.Link color="inherit" href="/login">

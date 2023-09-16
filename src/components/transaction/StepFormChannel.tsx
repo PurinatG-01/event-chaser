6f4f53927a4dab99654f539b988a33bd66a79fae
Purinat Sanbundit <37452245+PurinatG-01@users.noreply.github.com>
2023-09-16 16:52:15 +0700

    feat: update StepFormChannel in transaction

--- src/components/transaction/StepFormChannel.tsx
+++ src/components/transaction/StepFormChannel.tsx
@@ -1,11 +1,40 @@
-import React from "react"
+"use client"
+import React, { useEffect } from "react"
 import { BaseTransactionStepProps, StepFooter } from "./StepFooter"
+import {
+  TRANSACTION_CHANNEL,
+  useTransactionStep,
+} from "~/provider/useTransactionStep"
 
 export default function StepSummary(props: Props) {
   const { onGoPrev, onGoNext } = props
+  const { channel, charge, purchaseTicket } = useTransactionStep()
+
+  useEffect(() => {
+    if (!!charge) return
+    purchaseTicket()
+  }, [])
 
   return (
     <div className="flex flex-col flex-grow">
+      {!!charge && channel == TRANSACTION_CHANNEL.PROMPTPAY ? (
+        <div className="flex flex-col mx-auto">
+          <span className="text-lg font-bold mb-2">Charge id: {charge.id}</span>
+          <span className="text-lg font-bold mb-2">
+            Charge amount: {charge.amount / 100} Baht
+          </span>
+          <span className="text-lg font-bold mb-2">
+            Charge status: {charge.status}
+          </span>
+          <img
+            className="max-w-[400px] w-full rounded-sm overflow-hidden"
+            src={charge.source.scannable_code.image.download_uri}
+            alt="QR Promptpay code"
+          />
+        </div>
+      ) : (
+        <>Error occured: Cannot created charge</>
+      )}
       <StepFooter
         onGoPrev={onGoPrev}
         onGoNext={onGoNext}
--- src/provider/useTransactionStep.tsx
+++ src/provider/useTransactionStep.tsx
@@ -1,19 +1,31 @@
+import { channel } from "diagnostics_channel"
+import { useParams } from "next/navigation"
 import React, { createContext, useContext, useEffect, useState } from "react"
+import { Charge } from "~/models/Charge"
+import query from "~/utils/query"
 
 export enum TRANSACTION_CHANNEL {
   PROMPTPAY = "promptpay",
   CREDIT_CARD = "credit-card",
 }
 
+export const MAX_TICKET_PER_TRANSACTION = 5
+
 interface TransactionStateActions {
   setTransactionChannel: (channel: TRANSACTION_CHANNEL) => void
   setTransactionSuccess: (isPurchaseSuccess: boolean) => void
   setPassedStep: (passedStep: number) => void
+  setEventId: (eventId: string) => void
+  setAmount: (amount: number) => void
+  purchaseTicket: () => void
 }
 
 interface TransactionState {
   channel: TRANSACTION_CHANNEL | ""
+  eventId: string
+  amount: number
   isPurchaseSuccess: boolean
+  charge: Charge | null
   //   starting from 0
   passedStep: number
 }
@@ -22,7 +34,10 @@ interface ProviderProps extends React.PropsWithChildren<{}> {}
 
 const defaultTransactionState: TransactionState = {
   channel: "",
+  eventId: "",
+  amount: 1,
   isPurchaseSuccess: false,
+  charge: null,
   passedStep: 0,
 }
 
@@ -33,6 +48,9 @@ const TransactionStepContext = createContext<
   setTransactionChannel: (channel: TRANSACTION_CHANNEL) => {},
   setTransactionSuccess: (isPurchaseSuccess: boolean) => {},
   setPassedStep: (passedStep: number) => {},
+  setAmount: (amount: number) => {},
+  setEventId: (eventId: string) => {},
+  purchaseTicket() {},
 })
 
 export const useTransactionStep = (): TransactionState &
@@ -42,6 +60,7 @@ export default function TransactionStepProvider(props: ProviderProps) {
   const [transactionState, setTransactionState] = useState<TransactionState>(
     defaultTransactionState
   )
+  const params = useParams()
 
   const setTransactionChannel = (channel: TRANSACTION_CHANNEL) => {
     setTransactionState((prev) => ({ ...prev, channel }))
@@ -55,9 +74,54 @@ export default function TransactionStepProvider(props: ProviderProps) {
     setTransactionState((prev) => ({ ...prev, passedStep }))
   }
 
+  const setEventId = (eventId: string) => {
+    setTransactionState((prev) => ({ ...prev, eventId }))
+  }
+
+  const setAmount = (amount: number) => {
+    if (amount > MAX_TICKET_PER_TRANSACTION && amount <= 0) return
+    setTransactionState((prev) => ({ ...prev, amount }))
+  }
+
+  const resolverCharge = (charge: Charge) => {
+    if (transactionState.channel == TRANSACTION_CHANNEL.PROMPTPAY) {
+      setTransactionState((prev) => ({ ...prev, charge }))
+    } else {
+    }
+  }
+
+  const purchaseTicket = async () => {
+    try {
+      const formValue = {
+        eventId: transactionState.eventId,
+        amount: `${transactionState.amount}`,
+        channel: transactionState.channel,
+      }
+      const formData = new FormData()
+      for (const [key, value] of Object.entries(formValue)) {
+        formData.append(key, value)
+      }
+      const response = await query()<{ charge: Charge}>("/payment/purchase/ticket", {
+        method: "POST",
+        data: formData,
+      })
+      if (response.data.data.charge) {
+        resolverCharge(response.data.data.charge)
+      } else {
+        throw new Error("No charge data")
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   useEffect(() => {
     if (!!transactionState.channel) setPassedStep(1)
   }, [transactionState.channel])
+
+  useEffect(() => {
+    setEventId(params.id as string)
+  }, [])
 
   return (
     <TransactionStepContext.Provider
@@ -66,6 +130,9 @@ export default function TransactionStepProvider(props: ProviderProps) {
         setTransactionChannel,
         setTransactionSuccess,
         setPassedStep,
+        purchaseTicket,
+        setEventId,
+        setAmount,
       }}
     >
       {props.children}
